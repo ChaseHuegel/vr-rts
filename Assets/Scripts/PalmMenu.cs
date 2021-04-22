2269f82f9d91a0e2153fc5bb7266ad1494acc47e
incrediclint <incrediclint@gmail.com>
2021-04-22 19:26:02 -0400

    3D menu/board attached to arm now
    No toggle for it yet!

--- Assets/Scripts/PalmMenu.cs
+++ Assets/Scripts/PalmMenu.cs
@@ -5,20 +5,24 @@ using Valve.VR.InteractionSystem;
 
 public class PalmMenu : MonoBehaviour
 {
-    public Hand leftHand;
-    public Hand rightHand;
+    // public Hand leftHand;
+    // public Hand rightHand;
 
     Hand menuHand;
     Hand selectionHand;
 
     public GameObject prefabCube;
     public GameObject prefabSphere;
+    public GameObject prefabMage;
 
+    public GameObject prefabKnight;
+    public GameObject prefabFatKnight;
+    
     // Start is called before the first frame update
     void Start()
     {
-        menuHand = leftHand;
-        selectionHand = rightHand;
+        menuHand = Player.instance.leftHand;
+        selectionHand = Player.instance.rightHand;
     }
 
     // Update is called once per frame
@@ -30,23 +34,29 @@ public class PalmMenu : MonoBehaviour
     public void SpawnCube()
     {
         GameObject a = Instantiate(prefabCube) as GameObject;
-
-        selectionHand.AttachObject(a, GrabTypes.Trigger, Hand.AttachmentFlags.ParentToHand |
-                                                              Hand.AttachmentFlags.DetachOthers |
-                                                              Hand.AttachmentFlags.DetachFromOtherHand |
-                                                              Hand.AttachmentFlags.TurnOnKinematic |
-                                                              Hand.AttachmentFlags.TurnOffGravity |
-                                                              Hand.AttachmentFlags.SnapOnAttach);
+        selectionHand.AttachObject(a, GrabTypes.Scripted);
     }
 
     public void SpawnSphere()
     {
         GameObject a = Instantiate(prefabSphere) as GameObject;
-        selectionHand.AttachObject(a, GrabTypes.Trigger, Hand.AttachmentFlags.ParentToHand |
-                                                              Hand.AttachmentFlags.DetachOthers |
-                                                              Hand.AttachmentFlags.DetachFromOtherHand |
-                                                              Hand.AttachmentFlags.TurnOnKinematic |
-                                                              Hand.AttachmentFlags.TurnOffGravity |
-                                                              Hand.AttachmentFlags.SnapOnAttach);
+        selectionHand.AttachObject(a, GrabTypes.Scripted);
+    }
+
+    public void SpawnMage()
+    {
+        GameObject a = Instantiate(prefabMage) as GameObject;
+        selectionHand.AttachObject(a, GrabTypes.Scripted);
+    }
+
+    public void SpawnKnight()
+    {
+        GameObject a = Instantiate(prefabKnight) as GameObject;
+        selectionHand.AttachObject(a, GrabTypes.Scripted);
+    }
+    public void SpawnFatKnight()
+    {
+        GameObject a = Instantiate(prefabFatKnight) as GameObject;
+        selectionHand.AttachObject(a, GrabTypes.Scripted);
     }
 }
