ab7cde87d3cfe68284552c3db78c12f930c70cfe
incrediclint <incrediclint@gmail.com>
2021-06-04 17:53:50 -0400

    Revert "LaunchProjectile bug when target is destroyed."
    This reverts commit 2483ebb613fec0a7d6e03a0e58dc2c9043997d00.

--- Assets/Scripts/RTSCore/Unit.cs
+++ Assets/Scripts/RTSCore/Unit.cs
@@ -206,17 +206,13 @@ public class Unit : Actor, IFactioned
 
     public virtual void LaunchProjectile()
     {
-        if (!projectile)
+        if (!projectile && projectileTarget)
         {
             projectile = Instantiate(rangedProjectile);
             projectile.transform.position = transform.position;
             projectile.transform.position += new Vector3(0, 0.09f, 0);
-
-            if (projectileTarget)
-            {
-                projectileTargetPos = projectileTarget.transform.position;
-                projectileTargetPos += new Vector3(0, 0.09f, 0);
-            }
+            projectileTargetPos = projectileTarget.transform.position;
+            projectileTargetPos += new Vector3(0, 0.09f, 0);
         }
 
         // First we get the direction of the arrow's forward vector to the target position.
