b8d954a2b3ea97786699557f4cda5f37e4720f51
incrediclint <incrediclint@gmail.com>
2022-11-11 15:57:51 -0500

    Created FactionedResource inherited class

---
+++ Assets/Scripts/FactionedResource.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Changes the material for units and buildings based on the color chosen by 
+/// the player for the objects it's attached to.
+/// </summary>
+public class FactionedResource : Resource
+{
+    protected override void UpdateSkin()
+    {
+        if (SkinRendererTargets.Length <= 0) return;
+
+        if (Faction?.skin?.buildingMaterial)
+        {
+            foreach (var renderer in SkinRendererTargets)
+                renderer.sharedMaterial = Faction.skin.buildingMaterial;
+        }
+    }
+}
