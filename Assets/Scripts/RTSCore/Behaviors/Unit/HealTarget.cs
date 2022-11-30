cfd8ccebd1c23859abbe37ec147acaee32da1d4d
incrediclint <incrediclint@gmail.com>
2022-11-30 11:51:02 -0500

    Priest healing dead units fixed

--- Assets/Scripts/RTSCore/Behaviors/Unit/HealTarget.cs
+++ Assets/Scripts/RTSCore/Behaviors/Unit/HealTarget.cs
@@ -6,17 +6,8 @@ public class HealTarget : BehaviorNode<UnitV2>
     public override BehaviorState Evaluate(UnitV2 unit, float delta)
     {
         unit.HealingTarget = !unit.Target.Attributes.Get(AttributeType.HEALTH).IsMax();
-
-        // Debug.Log(unit.Target.Attributes.ValueOf(AttributeType.HEALTH));
-        // Debug.Log(unit.Target.Attributes.MaxValueOf(AttributeType.HEALTH));
-
-        // Debug.Log(unit.Target.Attributes.Get(AttributeType.HEALTH).MaxValue.ToString());
-        // Debug.Log(unit.Target.Attributes.Get(AttributeType.HEALTH).Value.ToString());
-
-        Debug.LogFormat("{0} : {1}/{2}", unit.Target.name,
-            unit.Target.Attributes.ValueOf(AttributeType.HEALTH),
-            unit.Target.Attributes.MaxValueOf(AttributeType.HEALTH));
-
+        unit.HealingTarget &= unit.Target.IsAlive();
+        
         return unit.HealingTarget ? BehaviorState.RUNNING : BehaviorState.SUCCESS;
     }
 }
