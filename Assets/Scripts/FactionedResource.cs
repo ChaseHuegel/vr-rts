85aee3ce3077850b0e00627c0ef36aee140df48e
incrediclint <incrediclint@gmail.com>
2022-12-08 17:36:36 -0500

    Update FactionedResource.cs

--- Assets/Scripts/FactionedResource.cs
+++ Assets/Scripts/FactionedResource.cs
@@ -30,7 +30,27 @@ public class FactionedResource : Resource
             foreach (StatUpgradeContainer bonus in bonuses)
                 Attributes.Get(bonus.targetAttribute).AddModifier(bonus.targetAttribute, bonus.modifier, bonus.amount);
     }
-    
+
+    public override float TryRemove(float count)
+    {
+        Attributes.Get(AttributeType.YIELD).Value -= count;
+        float yield = Attributes.Get(AttributeType.YIELD).Value;
+
+        float overflow = yield < 0 ? Mathf.Abs(yield) : 0;
+
+        if (yield <= 0)
+        {
+            UnbakeFromGrid();
+
+            if (onDestroyPrefab)
+                Instantiate(onDestroyPrefab, this.transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
+
+        return count - overflow;
+    }
+
     protected override void UpdateSkin()
     {
         if (SkinRendererTargets.Length <= 0) return;
